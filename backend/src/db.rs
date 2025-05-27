355d6a24f4f00cd122307a2b6e31d304f840998a
Besgood <kevindcriswell@gmail.com>
2025-05-26 21:03:20 -0400

    Delete backend directory

--- backend/src/db.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::postgres::PgPoolOptions;
-
-pub async fn init_db() -> Result<sqlx::PgPool, sqlx::Error> {
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
-    let pool = PgPoolOptions::new().connect(&database_url).await?;
-    Ok(pool)
-}
